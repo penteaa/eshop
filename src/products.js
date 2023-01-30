62b8a833b1e0a1a7680f8ddca870aeafdf777455
Penteaa <p_beny_04@yahoo.com>
2023-01-30 17:33:04 +0200

    first commit

---
+++ src/products.js
@@ -0,0 +1,59 @@
+import product1 from "./assets/products/1.webp";
+import product2 from "./assets/products/2.webp";
+import product3 from "./assets/products/3.jpg";
+import product4 from "./assets/products/4.webp";
+import product5 from "./assets/products/5.jpg";
+import product6 from "./assets/products/6.webp";
+import product7 from "./assets/products/7.webp";
+import product8 from "./assets/products/8.webp";
+
+export const PRODUCTS = [
+  {
+    id: 1,
+    productName: "IPhone 11 White",
+    price: 999.0,
+    productImage: product1,
+  },
+  {
+    id: 2,
+    productName: "Motorola Edge",
+    price: 200.0,
+    productImage: product2,
+  },
+  {
+    id: 3,
+    productName: "IPhone 11 Black",
+    price: 699.0,
+    productImage: product3,
+  },
+  {
+    id: 4,
+    productName: "Iphone XS",
+    price: 299.0,
+    productImage: product4,
+  },
+  {
+    id: 5,
+    productName: "Iphone 11 Black 128GB",
+    price: 500.99,
+    productImage: product5,
+  },
+  {
+    id: 6,
+    productName: "Iphone 11 White 128GB",
+    price: 68.0,
+    productImage: product6,
+  },
+  {
+    id: 7,
+    productName: "Motorola Edge 20 Lite",
+    price: 199.0,
+    productImage: product7,
+  },
+  {
+    id: 8,
+    productName: "IPhone 12 Mini",
+    price: 300.0,
+    productImage: product8,
+  },
+];
